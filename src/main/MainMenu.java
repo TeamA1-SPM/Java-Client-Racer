909c892cbbcbde1a3a9e817b997b1cb4e37902c9
kzuber93 <KevinZuber93@gmail.com>
2023-12-05 15:21:08 +0100

    Implementation of Multiplayer Window + Refactoring

--- src/main/MainMenu.java
+++ src/main/MainMenu.java
@@ -10,54 +10,83 @@ import java.util.Objects;
 
 public class MainMenu extends JFrame implements ActionListener {
 
-    private final JLabel labelBackground = new JLabel();
-    private final JButton playButton = new JButton("PLAY");
-    private final JButton multiplayerButton = new JButton("MULTIPLAYER");
-    private final JButton exitButton = new JButton("EXIT");
+    private final JLabel mainMenuLabel = new JLabel("MAIN MENU");
+    private final JLabel multiplayerLabel = new JLabel("MULTIPLAYER");
+    private final JButton playButton = new JButton();
+    private final JButton multiplayerButton = new JButton();
+    private final JButton exitButton = new JButton();
+    private final JButton loginButton = new JButton();
+    private final JButton registerButton = new JButton();
+    private final JButton backButton = new JButton();
     private ImageIcon background;
     private PlayButtonListener playButtonListener;
+    private final int xBTN = 350;
+    private final int yBTN = 150;
+    private final int widthBTN = 280;
+    private final int heightBTN = 60;
 
     public MainMenu() {
         init();
     }
+
     public void init(){
-        setupButtons();
+        setupButton(playButton, xBTN, yBTN, widthBTN, heightBTN, "Play");
+        setupButton(multiplayerButton, xBTN, yBTN + 75, widthBTN, heightBTN, "Multiplayer");
+        setupButton(exitButton, xBTN, yBTN + 150, widthBTN, heightBTN, "Exit");
+
+        setupButton(loginButton, xBTN, yBTN, widthBTN, heightBTN, "Login");
+        setupButton(registerButton, xBTN, yBTN + 75, widthBTN, heightBTN, "Register");
+        setupButton(backButton, xBTN, yBTN + 150, widthBTN, heightBTN, "Back");
+
         setupBackground();
         setupMainMenu();
     }
-    // Sets up the buttons
-    public void setupButtons() {
-        // Sets up the play button
-        playButton.setBounds(350,150,280,60);
-        playButton.setFont(new Font("Calibri", Font.BOLD, 40));
-        playButton.addActionListener(this);
-
-        // Sets up the multiplayer button
-        multiplayerButton.setBounds(350,225,280,60);
-        multiplayerButton.setFont(new Font("Calibri", Font.BOLD, 40));
-        multiplayerButton.addActionListener(this);
-
-        // Sets up the exit button
-        exitButton.setBounds(350,300,280,60);
-        exitButton.setFont(new Font("Calibri", Font.BOLD, 40));
-        exitButton.addActionListener(this);
+    // Sets up the button
+    public void setupButton(JButton button, int x, int y, int width, int height, String text) {
+        button.setBounds(x, y, width, height);
+        button.setFont(new Font("Calibri", Font.BOLD, 40));
+        button.setText(text);
+        button.addActionListener(this);
     }
+
+    public void addButtonToBackground(JLabel label, JButton button) {
+        label.add(button);
+    }
+
+    public void setVisibilityOfButton(JButton button, boolean isVisible) {
+        button.setVisible(isVisible);
+    }
+
+    public void setTextOfLabel(JLabel label, String text) {
+        label.setText(text);
+    }
+
 
     // Sets up the background image and corresponding text
     public void setupBackground() {
         background = new ImageIcon(Objects.requireNonNull(getClass().getResource("/main/images/menu/LobbyBackground.png")));
-        labelBackground.setFont(new Font("Calibri", Font.BOLD, 65));
-        labelBackground.setText("MAIN MENU");
-        labelBackground.setHorizontalTextPosition(JLabel.CENTER);
-        labelBackground.setVerticalTextPosition(JLabel.TOP);
-        labelBackground.setIconTextGap(-150);
-        labelBackground.setIcon(background);
+        mainMenuLabel.setFont(new Font("Calibri", Font.BOLD, 65));
+        mainMenuLabel.setHorizontalTextPosition(JLabel.CENTER);
+        mainMenuLabel.setVerticalTextPosition(JLabel.TOP);
+        mainMenuLabel.setIconTextGap(-150);
+        mainMenuLabel.setIcon(background);
 
         // Adds the buttons to the background image
-        labelBackground.add(playButton);
-        labelBackground.add(multiplayerButton);
-        labelBackground.add(exitButton);
+        addButtonToBackground(mainMenuLabel, playButton);
+        addButtonToBackground(mainMenuLabel, multiplayerButton);
+        addButtonToBackground(mainMenuLabel, exitButton);
+
+        addButtonToBackground(mainMenuLabel, loginButton);
+        addButtonToBackground(mainMenuLabel, registerButton);
+        addButtonToBackground(mainMenuLabel, backButton);
+
+        setVisibilityOfButton(loginButton,false);
+        setVisibilityOfButton(registerButton,false);
+        setVisibilityOfButton(backButton,false);
+
+        mainMenuLabel.add(multiplayerLabel);
     }
+
     // Sets up the main menu
     public void setupMainMenu() {
         this.setSize(background.getIconWidth(), background.getIconHeight());
@@ -65,7 +94,7 @@ public class MainMenu extends JFrame implements ActionListener {
         this.setResizable(Settings.SCREEN_RESIZABLE);
         this.setVisible(Settings.SCREEN_VISIBLE);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.add(labelBackground);
+        this.add(mainMenuLabel);
         this.pack();
         this.setLocationRelativeTo(null);
     }
@@ -83,9 +112,27 @@ public class MainMenu extends JFrame implements ActionListener {
             }
             dispose();
         } else if (e.getSource() == multiplayerButton) {
+            setVisibilityOfButton(playButton,false);
+            setVisibilityOfButton(multiplayerButton,false);
+            setVisibilityOfButton(exitButton,false);
 
+            setVisibilityOfButton(loginButton,true);
+            setVisibilityOfButton(registerButton,true);
+            setVisibilityOfButton(backButton,true);
+
+            setTextOfLabel(mainMenuLabel, "MULTIPLAYER");
         } else if (e.getSource() == exitButton) {
             System.exit(0);
+        } else if (e.getSource() == backButton) {
+            setVisibilityOfButton(loginButton,false);
+            setVisibilityOfButton(registerButton,false);
+            setVisibilityOfButton(backButton,false);
+
+            setVisibilityOfButton(playButton,true);
+            setVisibilityOfButton(multiplayerButton,true);
+            setVisibilityOfButton(exitButton,true);
+
+            setTextOfLabel(mainMenuLabel, "MAIN MENU");
         }
     }
 }
